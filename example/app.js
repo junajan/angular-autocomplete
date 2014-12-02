aff4cd59c64ea3c0dd1f2495d6b2e9a7d4acd8dc
junajan <jeezaa@seznam.cz>
2014-12-02 19:41:01 +0200

    Reworked and added some functionality

--- example/app.js
+++ example/app.js
@@ -1,26 +1,30 @@
-var app = angular.module("MyApp", ["LiveSearch"]);
+var app = angular.module("MyApp", ["ngAutocomplete"]);
 app.controller("MyController", function($scope, $http, $q, $window) {
     
-    $scope.mySearch = "";
-    
+    $scope.mySearch = {};
+
     $scope.mySearchCallback = function(params) {
 
       var defer = $q.defer();
-
+      console.log(params);
       $http.jsonp("http://gd.geobytes.com/AutoCompleteCity?callback=JSON_CALLBACK&q=" + params.query)
         .then(function(response) {
+          var cities = [];
+
           if(!response.data) {
             defer.resolve([]);
           }
-          var cities = response.data.map(function(city) {
-            var parts = city.split(",");
-            return {
-              fullName: city,
-              city: parts[0],
-              state: parts[1],
-              country: parts[2]
-            };
-          });
+          console.log(response.data);
+          if(response.data.length && response.data[0] != "")
+            cities = response.data.map(function(city) {
+              var parts = city.split(",");
+              return {
+                fullName: city,
+                city: parts[0],
+                state: parts[1],
+                country: parts[2]
+              };
+            });
           defer.resolve(cities);
         })
         .catch(function(e) {
---
+++ ngAutocomplete.js
@@ -0,0 +1,146 @@
+'use strict';
+
+angular.module("ngAutocomplete", ["ng"])
+ .directive("autocomplete", ["$compile", "$timeout", "$document", function ($compile, $timeout, $document) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            searchCallback: '=',
+            attributeName: '=?',
+            searchDelay: '=?',
+            maxResultSize: '=?',
+            ngModel: '='
+        },
+        template: "<span class='livesearch'><input type='text' ng-blur='blurInput()' /></span>",
+        link: function (scope, element, attrs, controller) {
+            var timeout;
+            var minLen = 1;
+            var maxLen = 9;
+            var inputElement;
+
+            scope.results = [];
+            scope.visible = false;
+            scope.selectedIndex = -1;
+
+            $document.on("click", function () {
+              scope.$apply(function() {
+                scope.$eval(scope.hideResults);
+              });
+            });
+
+            scope.hideResults = function() {
+                scope.visible = false;
+            };
+
+            scope.blurInput = function() {
+                scope.ngModel = {};
+            };
+
+            scope.select = function (index) {
+                scope.selectedIndex = index;
+                scope.visible = false;
+            };
+
+            scope.isSelected = function (index) {
+                return (scope.selectedIndex === index);
+            };
+
+            scope.init = function() {
+                
+                inputElement = element.find("input");
+
+                scope.$watch("selectedIndex", function(newValue, oldValue) {
+                    var item = scope.results[newValue];
+                    if(item) {
+                        if(attrs.attributeName) {
+                            inputElement.val(item[attrs.attributeName]);
+                        }
+                        else {
+                            inputElement.val(item);
+                        }
+                    }
+                    scope.ngModel = item;
+                });
+
+                scope.$watch("visible", function(newValue, oldValue) {
+                    if(newValue == false) {
+                        return;
+                    }
+                });
+
+
+                inputElement[0].onkeydown = function (e) {
+                    //keydown
+                    if (e.keyCode == 40) {
+                        if(scope.selectedIndex + 1 === scope.results.length) {
+                            scope.selectedIndex = 0;
+                        }
+                        else {
+                            scope.selectedIndex++;
+                        }
+                    }
+                    //keyup
+                    else if (e.keyCode == 38) {
+                        if(scope.selectedIndex == 0) {
+                            scope.selectedIndex = scope.results.length - 1;    
+                        }
+                        else if(scope.selectedIndex == -1) {
+                            scope.selectedIndex = 0;
+                        }
+                        else scope.selectedIndex--;
+                    }
+                    //keydown or keyup
+                    if (e.keyCode == 13) {
+                        scope.visible = false;
+                    }
+
+                    //unmanaged code needs to force apply
+                    scope.$apply();
+                };
+
+                inputElement[0].onkeyup = function (e) {
+                    if (e.keyCode == 13 || e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) {
+                        return false;
+                    }
+                    var target = element.find("input");
+                    // Set Timeout
+                    $timeout.cancel(timeout);
+                    // Set Search String
+                    var vals = target.val().split(",");
+                    var search_string = vals[vals.length - 1].trim();
+                    // Do Search
+                    if (search_string.length < minLen || search_string.length > maxLen) {
+                        scope.visible = false;
+                        //unmanaged code needs to force apply
+                        scope.$apply();
+                        return;
+                    }
+                    timeout = $timeout(function () {
+                        var results = [];
+                        var promise = scope.searchCallback.call(null, { query: search_string });
+                        promise.then(function (dataArray) {
+                            if (dataArray) {
+                                results = dataArray.slice(0, (scope.maxResultSize || 20) - 1);
+                            }
+                            if(results.length)
+                                scope.visible = true;
+                        });
+                        promise.finally(function() {
+                            scope.selectedIndex = -1;
+                            scope.results = results.filter(function(elem, pos) {
+                                return results.indexOf(elem) == pos;
+                            });
+                        });
+                    }, scope.searchDelay || 100);
+                };
+            };
+
+            var itemTemplate = element.attr("result-template") || "{{result}}";
+            var template = "<ul ng-show='visible && results.length' class='searchresultspopup'><li ng-class=\"{ 'selected' : isSelected($index) }\" ng-click='select($index)' ng-repeat='result in results'>" + itemTemplate + "</li></ul>";
+            var searchPopup = $compile(template)(scope);
+            element.append(searchPopup[0]);
+            $timeout(scope.init);
+        }
+    };
+}]);
